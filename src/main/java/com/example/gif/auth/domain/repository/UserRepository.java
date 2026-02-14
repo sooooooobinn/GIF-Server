8d7e409abbff67ca58e8e0ede253ab4625733d6f
user <sooooooobinn@gmail.com>
2026-02-14 20:34:04 +0900

    feat: providerId 값 추가

--- src/main/java/com/example/gif/auth/domain/repository/UserRepository.java
+++ src/main/java/com/example/gif/auth/domain/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmailAndProvider(String email, String provider);
+    Optional<User> findByProviderAndProviderId(User.Provider provider, String email);
 }
