145138949d8ca72df31354934bafe90f948ae84d
user <sooooooobinn@gmail.com>
2026-03-04 14:00:05 +0900

    feat: 추가 정보 입력 controller 추가

--- src/main/java/com/example/gif/auth/domain/controller/LoginController.java
+++ src/main/java/com/example/gif/auth/domain/controller/LoginController.java
@@ -1,11 +1,24 @@
 package com.example.gif.auth.domain.controller;
 
+import com.example.gif.auth.domain.dto.AdminAdditionalInfo;
+import com.example.gif.auth.domain.dto.ClientAdditionalInfo;
+import com.example.gif.auth.domain.entity.User;
+import com.example.gif.auth.domain.repository.UserRepository;
+import com.example.gif.auth.domain.service.OAuth2Service;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class LoginController {
+
+    private final OAuth2Service service;
+    private final UserRepository userRepository;
 
     @GetMapping("/admin/login")
     public String adminLogin(HttpServletRequest request) {
@@ -17,5 +30,45 @@ public class LoginController {
     public String clientLogin(HttpServletRequest request) {
         request.getSession().setAttribute("loginType", "client");
         return "redirect:/oauth2/authorization/google";
+    }
+
+    @PostMapping("/client/additional-info")
+    public String completeClientInfo(Authentication authentication, @RequestBody ClientAdditionalInfo request) {
+
+        if(authentication == null) {
+            throw new IllegalStateException("인증 정보 없음");
+        }
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String providerId = oAuth2User.getName();
+
+        User user = userRepository.findByProviderAndProviderId(
+                User.Provider.GOOGLE,
+                providerId
+        ).orElseThrow(() -> new IllegalArgumentException("유저 없음"));
+
+        service.completeClientInfo(user.getId(), request);
+
+        return "client 정보 저장 완료";
+    }
+
+    @PostMapping("/admin/additional-info")
+    public String completeAdminInfo(Authentication authentication, @RequestBody AdminAdditionalInfo request) {
+
+        if(authentication == null) {
+            throw new IllegalStateException("인증 정보 없음");
+        }
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String providerId = oAuth2User.getName();
+
+        User user = userRepository.findByProviderAndProviderId(
+                User.Provider.GOOGLE,
+                providerId
+        ).orElseThrow(() -> new IllegalArgumentException("유저 없음"));
+
+        service.completeAdminInfo(user.getId(), request);
+
+        return "admin 정보 저장 완료";
     }
 }
