15723f8e190ae2088a12ce3f624ef2459763312a
user <sooooooobinn@gmail.com>
2026-03-24 02:11:28 +0900

    update: controller 통합

--- src/main/java/com/example/gif/auth/domain/controller/LoginController.java
+++ src/main/java/com/example/gif/auth/domain/controller/LoginController.java
@@ -2,9 +2,8 @@ package com.example.gif.auth.domain.controller;
 
 import com.example.gif.auth.domain.dto.AdminAdditionalInfo;
 import com.example.gif.auth.domain.dto.ClientAdditionalInfo;
-import com.example.gif.auth.domain.entity.User;
-import com.example.gif.auth.domain.repository.UserRepository;
 import com.example.gif.auth.domain.service.OAuth2Service;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -15,39 +14,30 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
     private final OAuth2Service service;
-    private final UserRepository userRepository;
 
     @GetMapping("/admin/login")
-    public String adminLogin() {
-        return "redirect:/oauth2/authorization/google?loginType=admin";
+    public String adminLogin(HttpServletRequest request) {
+        request.getSession().setAttribute("loginType", "admin");
+        return "redirect:/oauth2/authorization/google";
     }
 
     @GetMapping("/client/login")
-    public String clientLogin() {
-        return "redirect:/oauth2/authorization/google?loginType=client";
+    public String clientLogin(HttpServletRequest request) {
+        request.getSession().setAttribute("loginType", "client");
+        return "redirect:/oauth2/authorization/google";
     }
 
     @PostMapping("/client/additional-info")
     public String completeClientInfo(Authentication authentication, @RequestBody ClientAdditionalInfo request) {
-
-        String providerId = authentication.getName();
-
-        User user = userRepository.findByProviderAndProviderId(User.Provider.GOOGLE, providerId)
-                .orElseThrow(() -> new IllegalArgumentException("유저 없음"));
-
-        service.completeClientInfo(user.getId(), request);
+        String providerId = authentication.getName(); // JWT 필터에서 가져온 ID
+        service.completeClientInfo(providerId, request);
         return "client 정보 저장 완료";
     }
 
     @PostMapping("/admin/additional-info")
     public String completeAdminInfo(Authentication authentication, @RequestBody AdminAdditionalInfo request) {
-
         String providerId = authentication.getName();
-
-        User user = userRepository.findByProviderAndProviderId(User.Provider.GOOGLE, providerId)
-                .orElseThrow(() -> new IllegalArgumentException("유저 없음"));
-
-        service.completeAdminInfo(user.getId(), request);
+        service.completeAdminInfo(providerId, request);
         return "admin 정보 저장 완료";
     }
 }
