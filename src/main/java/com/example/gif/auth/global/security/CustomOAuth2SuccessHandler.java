719cf2095439caed6627b4284d1547094826b813
user <sooooooobinn@gmail.com>
2026-03-23 21:15:58 +0900

    refactor: JwtToken 사용으로 코드 변경

--- src/main/java/com/example/gif/auth/global/security/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/example/gif/auth/global/security/CustomOAuth2SuccessHandler.java
@@ -2,6 +2,7 @@ package com.example.gif.auth.global.security;
 
 import com.example.gif.auth.domain.entity.User;
 import com.example.gif.auth.domain.repository.UserRepository;
+import com.example.gif.auth.global.security.jwt.JwtTokenProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import java.io.IOException;
 
 @Component
@@ -18,50 +19,34 @@ import java.io.IOException;
 public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication)
-
-        throws IOException, ServletException {
+            throws IOException, ServletException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
         String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
-        User.Provider provider = User.Provider.GOOGLE;
         String providerId = oAuth2User.getName();
 
-        String loginType =
-                (String) request.getSession().getAttribute("loginType");
+        User user = userRepository.findByProviderAndProviderId(User.Provider.GOOGLE, providerId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
 
-        if(loginType == null) {
-            response.sendRedirect("/login/error");
-            return;
-        }
+        String accessToken = jwtTokenProvider.createToken(
+                user.getProviderId(),
+                user.getEmail(),
+                user.getUserType().name()
+        );
 
-        User.UserType userType =
-                "admin".equalsIgnoreCase(loginType)
-                        ? User.UserType.ADMIN
-                        : User.UserType.CLIENT;
+        System.out.println("발급된 토큰: " + accessToken);
 
-        userRepository.findByProviderAndProviderId(provider, providerId)
-                .orElseGet(() ->
-                        userRepository.save(
-                                User.builder()
-                                        .username(name)
-                                        .email(email)
-                                        .provider(provider)
-                                        .providerId(providerId)
-                                        .userType(userType)
-                                        .role(null)
-                                        .build()
-                        )
-                );
+        String targetUrl = UriComponentsBuilder.fromUriString("/oauth/loginInfo")
+                .queryParam("accessToken", accessToken)
+                .build().toUriString();
 
-        request.getSession().removeAttribute("loginType");
-
-        response.sendRedirect("/oauth/loginInfo");
+        response.sendRedirect(targetUrl);
     }
 }
