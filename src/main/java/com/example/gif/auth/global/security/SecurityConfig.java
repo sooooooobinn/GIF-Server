463a4b1b659b85c90ff4223a2510f65ff4c207fe
user <sooooooobinn@gmail.com>
2026-03-23 21:14:57 +0900

    feat: JwtTokenProvider 사용

--- src/main/java/com/example/gif/auth/global/security/SecurityConfig.java
+++ src/main/java/com/example/gif/auth/global/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.gif.auth.global.security;
 
 import com.example.gif.auth.domain.service.OAuth2Service;
+import com.example.gif.auth.global.security.jwt.JwtAuthenticationFilter;
 import com.example.gif.auth.global.security.oauth.CustomAuthorizationRequestResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,23 +20,16 @@ public class SecurityConfig {
     private final OAuth2Service oAuth2Service;
     private final CustomOAuth2SuccessHandler successHandler;
     private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter; // 💡 추가
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .logout(logout -> logout.disable())
-                .formLogin(form -> form.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",
-                                "/auth/**",
-                                "/oauth2/**"
-                        ).permitAll()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/auth/**", "/oauth2/**", "/oauth/loginInfo").permitAll()
                         .anyRequest().authenticated()
                 )
-
-
                 .oauth2Login(oauth2 -> oauth2
                         .authorizationEndpoint(authorization -> authorization
                                 .baseUri("/oauth2/authorization")
@@ -44,6 +40,8 @@ public class SecurityConfig {
                         )
                         .successHandler(successHandler)
                 )
+
+                .addFilterBefore(jwtAuthenticationFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
