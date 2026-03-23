40d1c16121663a3985a9bb892f9c4975d3fb2d8f
user <sooooooobinn@gmail.com>
2026-03-23 21:14:44 +0900

    feat: JwtTokenProvider 추가

---
+++ src/main/java/com/example/gif/auth/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package com.example.gif.auth.global.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String salt;
+
+    private Key secretKey;
+
+    private final long exp = 1000L * 60 * 60;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(String providerId, String email, String role) {
+        Claims claims = Jwts.claims().setSubject(providerId);
+        claims.put("email", email);
+        claims.put("role", role);
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + exp))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getProviderId(String token) {
+        return Jwts.parserBuilder().setSigningKey(secretKey).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            log.info("JWT 토큰 유효성 검증 실패: {}", e.getMessage());
+            return false;
+        }
+    }
+}
