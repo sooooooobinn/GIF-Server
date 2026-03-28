3e1d5e4ad20713ac76ae666b3bdbe157e57651ad
hikeong <s25048@gsm.hs.kr>
2026-03-29 03:52:16 +0900

    refactor: FormResponseRepoitory 삭제

--- src/main/java/com/example/gif/form/domain/repository/FormResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gif.form.domain.repository;
-
-import com.example.gif.form.domain.dto.FormResponse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormResponseRepository extends JpaRepository<FormResponse, Long> {
-}
-
