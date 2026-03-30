4bf9a7c39cc6c706da9bc3335c699159382d9daf
user <sooooooobinn@gmail.com>
2026-03-31 00:41:21 +0900

    feat: RankResponseDto 생성자 추가

--- src/main/java/com/example/gif/score/dto/Response/RankResponseDto.java
+++ src/main/java/com/example/gif/score/dto/Response/RankResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.gif.score.dto.Response;
 
+import com.example.gif.score.entity.Score;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,10 @@ public class RankResponseDto {
     private String teamName;
     private Integer totalScore;
     private int rank;
+
+    public RankResponseDto(Score score) {
+        this.teamName = score.getTeamName().getProjectName();
+        this.totalScore = score.getTotalScore();
+        this.rank = score.getRank();
+    }
 }
