01b12d8c00f844bd3ca0426aec17851dd442106f
user <sooooooobinn@gmail.com>
2026-04-01 10:13:39 +0900

    feat: ScoreUpdateReqeustDto 추가

---
+++ src/main/java/com/example/gif/score/dto/Request/ScoreUpdateReqeustDto.java
@@ -0,0 +1,10 @@
+package com.example.gif.score.dto.Request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ScoreUpdateReqeustDto {
+    private Long scoreId;
+}
