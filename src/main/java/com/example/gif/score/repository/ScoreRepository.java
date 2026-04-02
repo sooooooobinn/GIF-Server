a1f9996e9b2f27a49dad33624e1157a1443a8589
user <sooooooobinn@gmail.com>
2026-04-02 17:25:01 +0900

    refactor: findByProject에 evaluatorId 추가 -> existsByProjectAndEvaluatorId

--- src/main/java/com/example/gif/score/repository/ScoreRepository.java
+++ src/main/java/com/example/gif/score/repository/ScoreRepository.java
@@ -4,9 +4,12 @@ import com.example.gif.project.entity.Project;
 import com.example.gif.score.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
     Optional<Score> findByProjectAndEvaluatorId(Project project, String evaluatorId);
+    boolean existsByProjectAndEvaluatorId(Project project, String evaluatorId);
 }
